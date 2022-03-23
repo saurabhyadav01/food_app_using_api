1e789b092a41e4008eb7254cae34d619035b4e2d
saurabh yadav <72351102+saurabhyadav01@users.noreply.github.com>
2022-03-23 08:34:03 +0530

    Update navbar.js

--- component/navbar.js
+++ component/navbar.js
@@ -7,8 +7,7 @@ function navbar()
     <div id="options">
       <p><a href="racipe.html">Day Racipe</a></p>
       <p><a href="latest.html">Latest Food</a></p>
-//       <p><a href="">Sign In</a></p>
-//       <p><a href="">Sign Up</a></p>
+
     </div>
   </div>`
 }
